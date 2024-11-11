d371a9d3d7227b4c60880a685145665b44e6d2b3
am-dp <155446024+am-dp@users.noreply.github.com>
2024-11-11 09:30:23 +0530

    new

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open(filename, 'rb') as file:
     regressor = pickle.load(file)
 
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
